12ae42ba896061fd98daf7c163089c666afc5425
Diego Fernandez <difernan@redhat.com>
2014-06-16 14:10:04 -0400

    fix installer

--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 rm -rf ~/.local/share/gnome-shell/extensions/return-to-monitor@aiguofer.github.com
-cp --parents metadata.json extension.js ~/.local/share/gnome-shell/extensions/return-to-monitor@aiguofer.github.com/
+mkdir -p ~/.local/share/gnome-shell/extensions/return-to-monitor@aiguofer.github.com
+cp metadata.json extension.js ~/.local/share/gnome-shell/extensions/return-to-monitor@aiguofer.github.com/
