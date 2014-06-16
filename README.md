ed8f5c8cd1d708ed9178503b40eb779ae5bf2455
Diego Fernandez <difernan@redhat.com>
2014-06-16 00:02:49 -0400

    added 3.10 and 'improved' installer

--- README.md
+++ README.md
@@ -2,3 +2,8 @@ return-to-monitor
 =================
 
 Gnome-Shell extension to return windows to the right monitor whenever you plug a monitor back in.
+
+To install (will remove a previous version and copy the new files in):
+
+cd /path/to/return-to-monitor
+./install.sh
--- install.sh
+++ install.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-cp -r `pwd` ~/.local/share/gnome-shell/extensions/return-to-monitor@aiguofer.github.com
+rm -rf ~/.local/share/gnome-shell/extensions/return-to-monitor@aiguofer.github.com
+cp --parents metadata.json extension.js ~/.local/share/gnome-shell/extensions/return-to-monitor@aiguofer.github.com/
